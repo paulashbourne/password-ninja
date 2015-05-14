3fb6aed9ccb5ee231a7bb6513cd22a0920202d1d
Paul Ashbourne <paul.ashbourne@hotmail.com>
2015-05-14 15:16:08 -0400

    Rebuilt extension

---
+++ extension/future/popup/popup.js
@@ -0,0 +1,69 @@
+require(
+    ['underscore', 'backbone'],
+    function(_, Backbone) {
+
+  var PopupView = Backbone.View.extend({
+    initialize : function() {
+      this.setElement($('body'));
+      //setup message port with background page
+      this.port = chrome.runtime.connect({'name' : 'popup'})
+      this.port.onMessage.addListener(_.bind(this.messageListener, this));
+    },
+    events : {
+      'click .popupButton'   : 'menuOptionSelected',
+    },
+    setData : function(data) {
+      //used to set variable values
+      $.extend(true, this, data);
+    },
+    setOptions : function(data) {
+      this.$el.html("");
+      var options = data.options;
+      console.log(options);
+      _.each(options, _.bind(function(option) {
+          console.log(option);
+        if (option.type == "div") {
+          //do something
+          console.log("do something");
+        } else {
+          this.$el.append(
+            '<div class="buttonRow">' +
+              '<a href="#" class="popupButton" data-value="'+option.value+'">' +
+              '<img class="buttonIcon" src="lock.png">' +
+                '<span>'+option.text+'</span>' +
+              '</a>' +
+            '</div>'
+          );
+        }
+      }, this));
+      this.delegateEvents();
+    },
+    menuOptionSelected : function(evt) {
+      this.messageBackground({
+        "menuOption" : $(evt.currentTarget).attr('data-value')
+      });
+      window.close();
+    },
+    messageListener : function(msg) {
+      console.log(msg);
+      if (typeof msg.command != "undefined") {
+        if (typeof msg.data != "undefined") {
+          this[msg.command](msg.data);
+        } else {
+          this[msg.command]();
+        }
+      }
+    },
+    messageBackground : function(msg) {
+      this.port.postMessage(msg);
+      console.log(msg);
+    }
+  });
+
+  // main
+  $(document).ready(function() {
+    var pageView = new PopupView();
+    pageView.render();
+  });
+
+});
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,69 +1,43 @@
 require(
-    ['underscore', 'backbone'],
-    function(_, Backbone) {
+    ['/lib/underscore.js', '/lib/backbone.js', '/lib/sha256.js'],
+    function(_, Backbone, sha256) {
 
   var PopupView = Backbone.View.extend({
     initialize : function() {
       this.setElement($('body'));
-      //setup message port with background page
-      this.port = chrome.runtime.connect({'name' : 'popup'})
-      this.port.onMessage.addListener(_.bind(this.messageListener, this));
+      this.render();
     },
     events : {
-      'click .popupButton'   : 'menuOptionSelected',
+      'click #generate-password'   : 'generatePassword',
     },
-    setData : function(data) {
-      //used to set variable values
-      $.extend(true, this, data);
-    },
-    setOptions : function(data) {
-      this.$el.html("");
-      var options = data.options;
-      console.log(options);
-      _.each(options, _.bind(function(option) {
-          console.log(option);
-        if (option.type == "div") {
-          //do something
-          console.log("do something");
-        } else {
-          this.$el.append(
-            '<div class="buttonRow">' +
-              '<a href="#" class="popupButton" data-value="'+option.value+'">' +
-              '<img class="buttonIcon" src="lock.png">' +
-                '<span>'+option.text+'</span>' +
-              '</a>' +
-            '</div>'
-          );
-        }
-      }, this));
-      this.delegateEvents();
-    },
-    menuOptionSelected : function(evt) {
-      this.messageBackground({
-        "menuOption" : $(evt.currentTarget).attr('data-value')
-      });
-      window.close();
-    },
-    messageListener : function(msg) {
-      console.log(msg);
-      if (typeof msg.command != "undefined") {
-        if (typeof msg.data != "undefined") {
-          this[msg.command](msg.data);
-        } else {
-          this[msg.command]();
-        }
+    generatePassword : function(evt) {
+      var domain = this.$('#domain').val();
+      if (domain == "") {
+        this.showResult("");
+        this.showError("Please enter the domain (e.g. facebook.com)");
+        return;
       }
+      var password = this.$('#password').val();
+      if (password == "") {
+        this.showResult("");
+        this.showError("Please enter your master password");
+        return;
+      }
+      var passwordHash = CryptoJS.SHA256(password+domain).toString().substring(0, 16);
+      this.showError("");
+      this.showResult("Your password is: " + passwordHash);
     },
-    messageBackground : function(msg) {
-      this.port.postMessage(msg);
-      console.log(msg);
+    showError : function(msg) {
+      this.$('#generate-error').html(msg);
+    },
+    showResult : function(msg) {
+      this.$('#generate-result').html(msg);
+    },
+    render : function() {
     }
   });
-
-  // main
-  $(document).ready(function() {
-    var pageView = new PopupView();
-    pageView.render();
-  });
-
+  var init_page = function() {
+    var popupView = new PopupView();
+  };  
+  $(document).ready(init_page);
 });
